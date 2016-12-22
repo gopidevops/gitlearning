406d95ae1f0361b9350d2961ec95a287525d063b
vishwa <bvish09@gmail.com>
2016-12-21 21:48:09 -0500

    committed powershell scripts

---
+++ math.psm1
@@ -0,0 +1,9 @@
+Clear-Host
+$1stNum = 22
+$2ndNum = 7
+Write-Host "Add, Subtract, Multiply and Divide"
+Write-Host "-----------------------------------"
+$1stNum + $2ndNum    #Add 
+$1stNum - $2ndNum     #Subtract 
+$1stNum * $2ndNum     #Multiply
+$1stNum / $2ndNum     #Divide 
